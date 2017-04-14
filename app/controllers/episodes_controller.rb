bb81fc360f90b7a8f9fdff38af0c22fe75b27502
Katarzyna Chachlowska <chachlowskak@gmail.com>
2017-04-14 17:25:30 +0200

    Add form for new episode

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -5,6 +5,19 @@ class EpisodesController < ApplicationController
     def new
         @welcome_string = "zyy".html_safe
     end
+
+    def create
+        @episode = Episode.new(episode_params)
+
+        @episode.save
+        redirect_to @episode
+
+      #  render plain: params[:episode].inspect
+    end
+
+    def show
+        
+    end
     
     def welcome_string
         "This is welcome string".html_safe
@@ -12,5 +25,12 @@ class EpisodesController < ApplicationController
     
     def url_length(string)
        string.length
+    end
+
+
+    private
+
+    def episode_params
+      params.require(:episode).permit(:episode_number, :title, :description, :date)
     end
 end
