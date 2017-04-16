d3a9c12458d39c64f1c314b411b6c681b8619dc3
Katarzyna Chachlowska <chachlowskak@gmail.com>
2017-04-16 21:08:50 +0200

    Create Song model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416162753) do
+ActiveRecord::Schema.define(version: 20170416185213) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20170416162753) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.date     "airing_date"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "artist",     limit: 255
+    t.string   "album",      limit: 255
+    t.integer  "year",       limit: 4
+    t.string   "length",     limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
